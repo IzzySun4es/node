4403f56f88f514287dcaab3ca8d09a1404af1c5f
Alexandra Chugaenko <sascha281201@gmain.com>
2023-12-15 13:51:21 +0300

    1

--- script.js
+++ script.js
@@ -55,6 +55,15 @@ app.get('/blogs',  function(req, res) {
             posts: data
         });
       });
+    // Post.findAll({raw: true, 
+        // include: [{
+        //             model: Tag,
+        //             attributes: ['name'], 
+        //          }],
+    // }).then((date)=>{
+    //     console.log(date)
+    //     res.render('blogs.hbs', {posts: date})
+    // })
 });
 // возвращаем форму для добавления данных
 app.get("/create", function(req, res){
@@ -78,7 +87,10 @@ app.post("/create", urlparser, function (req, res) {
             console.log(date)
             res.redirect('/blogs')
     });
-
+    // pool.query("INSERT INTO posts (name, descript, tag, date_post) VALUES (?,?,?,?)", [name, descript, tag, date_post], function(err, data) {
+    //   if(err) return console.log(err);
+    //   res.redirect("/blogs");
+    // });
 });
  
 // получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
@@ -92,6 +104,7 @@ app.get("/edit/:id", function(req, res){
      res.render("edit.hbs", {
         post: data[0]
     });
+    // res.render("create.hbs", { posts: data })
   });
 });
 // получаем отредактированные данные и отправляем их в БД
